c49291ca900c0f87552de6de31df20718b9d6b70
felipedorea <122095865+felipedorea@users.noreply.github.com>
2023-03-08 20:23:26 -0300

    .

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".d-1", {
+    strings:["WEB Developer", "Técnico de Informática", "WEB Designer", "Freelancer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop: true
+});
+
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    }
+}
